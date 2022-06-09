657369988f8c1fcbf60be71ed40b324543a729e7
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-08 23:45:22 -0500

    ModelMapper
    Model Mapper singleton implemented

--- src/main/java/com/sofkau/raulstorebe/RaulStoreBeApplication.java
+++ src/main/java/com/sofkau/raulstorebe/RaulStoreBeApplication.java
@@ -1,13 +1,19 @@
 package com.sofkau.raulstorebe;
 
+import com.sofkau.raulstorebe.mapper.StoreMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RaulStoreBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RaulStoreBeApplication.class, args);
+    }
+    @Bean
+    public StoreMapper StoreMapper() {
+        return new StoreMapper();
     }
 
 }
