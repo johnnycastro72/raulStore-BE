f805364cb3762534aa8f6d6472aceb9d43786b9e
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-06-08 22:48:05 -0500

    Initial commit

---
+++ src/main/java/com/sofkau/raulstorebe/RaulStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.raulstorebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaulStoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaulStoreBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/raulstorebe/RaulStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.raulstorebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaulStoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
